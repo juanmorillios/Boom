4c70a9e690564528c7fcd30fba64d4fef29eb99b
Juan Ml. Morillo <juanmorillios@gmail.com>
2015-10-28 19:24:01 +0100

    Change images of game
    Change images of game

--- Boom/ViewController.swift
+++ Boom/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgBlackBoom: UIImageView!
+    @IBOutlet weak var imgRedBoom: UIImageView!
+
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pressBtnHideBlackButton(sender: AnyObject) {
+        
+        imgBlackBoom.hidden = true
+       
+        
+    }
+
+    @IBAction func pressBtnHideBlueButton(sender: AnyObject) {
+        
+        
+        imgRedBoom.hidden = true
+
+        
+        
     }
 
 
